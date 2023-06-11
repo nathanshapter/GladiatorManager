8f7740c470288539a65e847cb28bcef0b58a7119
Nathan Shapter <shapternathan@protonmail.com>
2023-06-11 13:27:31 -0400

    enemy AI now hurts player when close, need to work on implementing battle screen

---
+++ Assets/EnemyGladiatorNPC.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGladiatorNPC : MonoBehaviour
+{
+    AIDestinationSetter ai;
+
+    private void Start()
+    {
+        ai = GetComponent<AIDestinationSetter>();
+        ai.target = FindObjectOfType<GladiatorNPC>().transform;
+    }
+
+    private void Update()
+    {
+        if(Vector2.Distance(this.transform.position, ai.target.transform.position) < 3)
+        {
+            ai.target.GetComponent<GladiatorNPC>().health -= 10;
+            print(ai.target.GetComponent<GladiatorNPC>().health);
+        }
+    }
+}

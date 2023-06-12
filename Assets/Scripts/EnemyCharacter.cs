b429e4fb34318067db080aca1aacdaec6c12875a
Nathan Shapter <shapternathan@protonmail.com>
2023-06-12 12:08:08 -0400

    battles are now randomized the amount of enemies, allies match

--- Assets/Scripts/EnemyCharacter.cs
+++ Assets/Scripts/EnemyCharacter.cs
@@ -11,7 +11,7 @@ public class EnemyCharacter : ScriptableObject
     public int strength , defence , intelligence , charisma , agility , endurance,accuracy, resilience , leadership , luck ;
     public int maxStat;
     public int minStat;
-
+   
 
     public GameObject slavePrefab;
 
@@ -19,6 +19,7 @@ public class EnemyCharacter : ScriptableObject
 
     private void OnEnable()
     {
+
         strength = Random.Range(0, 10);
         defence = Random.Range(0, 10);
         intelligence = Random.Range(0, 10);
@@ -29,9 +30,124 @@ public class EnemyCharacter : ScriptableObject
         resilience = Random.Range(0, 10);
         leadership = Random.Range(0, 10);
         luck = Random.Range(0, 10);
+
+        slaveName = ReturnName();
     }
     public void RandomizeStrength()
     {
         strength = Random.Range(0, 10);
     }
+
+    private string ReturnName()
+    {
+       string name = names[Random.Range(0, names.Length)];
+        return name;
+    }
+
+    string[] names = new string[]
+{
+    "Marcus",
+    "Lucius",
+    "Gaius",
+    "Titus",
+    "Quintus",
+    "Decimus",
+    "Publius",
+    "Tiberius",
+    "Julius",
+    "Flavius",
+    "Aulus",
+    "Cassius",
+    "Augustus",
+    "Valerius",
+    "Appius",
+    "Antonius",
+    "Caius",
+    "Claudius",
+    "Crassus",
+    "Decimus",
+    "Drusus",
+    "Felix",
+    "Fabius",
+    "Gnaeus",
+    "Horatius",
+    "Maximus",
+    "Marius",
+    "Octavius",
+    "Pompeius",
+    "Sextus",
+    "Varius",
+    "Vespasian",
+    "Agrippa",
+    "Marcellus",
+    "Nero",
+    "Otho",
+    "Servius",
+    "Sulla",
+    "Verus",
+    "Balbus",
+    "Glabrio",
+    "Cato",
+    "Crassus",
+    "Gallus",
+    "Lentulus",
+    "Mucius",
+    "Nasica",
+    "Paetus",
+    "Plancus",
+    "Pulcher",
+    "Quirinus",
+    "Rufus",
+    "Scipio",
+    "Silanus",
+    "Vergilius",
+    "Vibius",
+    "Hostus",
+    "Caecilius",
+    "Cincius",
+    "Hostilius",
+    "Calidius",
+    "Furio",
+    "Decius",
+    "Laelius",
+    "Quinctius",
+    "Salinator",
+    "Stolo",
+    "Aemilius",
+    "Curius",
+    "Flaccus",
+    "Volumnius",
+    "Vitellius",
+    "Messala",
+    "Blasius",
+    "Atilius",
+    "Cassius",
+    "Verginius",
+    "Vitruvius",
+    "Fulvius",
+    "Geminus",
+    "Manilius",
+    "Pontius",
+    "Vibius",
+    "Vopiscus",
+    "Accius",
+    "Agrippinus",
+    "Albinius",
+    "Barbatus",
+    "Caesennius",
+    "Egnatius",
+    "Hostius",
+    "Lentulus",
+    "Ovidius",
+    "Pansa",
+    "Quirinius",
+    "Rufinius",
+    "Statius",
+    "Tubero",
+    "Ulpius",
+    "Varro",
+    "Vinicius",
+    "Xanthus"
+    // Add more names here...
+};
 }
--- Assets/Scripts/SlaveBattleMenu.cs
+++ Assets/Scripts/SlaveBattleMenu.cs
@@ -6,17 +6,28 @@ using UnityEngine;
 public class SlaveBattleMenu : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI battle1, battle2, battle3;
-    [SerializeField] TextMeshProUGUI opponentInfo1, opponentInfo2, opponenentInfo3;
+    [SerializeField] TextMeshProUGUI opponentName1, opponentName2, opponenentName3;
     [SerializeField] EnemyCharacter[] allOpponents;
     [SerializeField] EnemyCharacter[] opponent1, opponent2, opponent3;
 
     private void Start()
     {
-        battle1.text = "1v1";
-        battle2.text = "1v5";
-        battle3.text = "1v2";
+        int amountOfEnemies = Random.Range(1, 5);
+        int amountOfCharacterSlaves = amountOfEnemies;
 
-        opponentInfo1.text = opponent1[0].name;
 
+
+        battle1.text = $"{amountOfCharacterSlaves}v{amountOfEnemies}";
+        amountOfEnemies = Random.Range(1, 5);
+        amountOfCharacterSlaves = amountOfEnemies;
+        battle2.text = $"{amountOfCharacterSlaves}v{amountOfEnemies}";
+        amountOfEnemies = Random.Range(1, 5);
+        amountOfCharacterSlaves = amountOfEnemies;
+        battle3.text = $"{amountOfCharacterSlaves}v{amountOfEnemies}";
+        
+        int numberOfEnemyCharacter = Random.Range(0, allOpponents.Length);
+
+        opponentName1.text = allOpponents[numberOfEnemyCharacter].name;
+        
     }
 }
